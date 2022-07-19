c8f2dbe137bb52f986d8efdab53b1a880fb338e7
albodell <106753318+albodell@users.noreply.github.com>
2022-07-19 16:25:09 -0400

    finish table

--- src/TableRow.js
+++ src/TableRow.js
@@ -1,4 +1,11 @@
-import { TableRow, TableCell, Paragraph } from "docx";
+import {
+  TableRow,
+  TableCell,
+  convertInchesToTwip,
+  AlignmentType,
+  WidthType,
+  TextRun
+} from "docx";
 
 export default function getTextPara(textElement) {
   var docx = require("docx");
@@ -11,7 +18,13 @@ export default function getTextPara(textElement) {
   for (var i = 0; i < cleanTextArray.length; ) {
     arrayOne.push(
       new docx.Paragraph({
-        text: cleanTextArray[i]
+        children: [
+          new TextRun({
+            font: "Bookman Old Style",
+            size: 19,
+            text: cleanTextArray[i]
+          })
+        ]
       })
     );
     i++;
@@ -21,7 +34,13 @@ export default function getTextPara(textElement) {
 
     arrayTwo.push(
       new docx.Paragraph({
-        text: cleanTextArray[i]
+        children: [
+          new TextRun({
+            font: "Bookman Old Style",
+            size: 19,
+            text: cleanTextArray[i]
+          })
+        ]
       })
     );
     i++;
@@ -31,19 +50,91 @@ export default function getTextPara(textElement) {
 
     arrayThree.push(
       new docx.Paragraph({
-        text: cleanTextArray[i]
+        children: [
+          new TextRun({
+            font: "Bookman Old Style",
+            size: 19,
+            text: cleanTextArray[i]
+          })
+        ]
       })
     );
     i++;
   }
 
-  var one = new TableCell({ children: arrayOne });
-  var two = new TableCell({ children: arrayTwo });
-  var three = new TableCell({ children: arrayThree });
+  var one = new TableCell({
+    children: arrayOne,
+    width: {
+      size: 33.3,
+      type: WidthType.PERCENTAGE
+    },
+    height: {
+      size: 0.2
+    },
+    margins: {
+      left: convertInchesToTwip(0.1),
+      right: convertInchesToTwip(0.1)
+    },
+    borders: {
+      left: {
+        size: 3
+      }
+    }
+  });
+
+  var two = new TableCell({
+    children: arrayTwo,
+    width: {
+      size: 33.3,
+      type: WidthType.PERCENTAGE
+    },
+    height: {
+      size: 0.2
+    },
+    margins: {
+      left: convertInchesToTwip(0.1),
+      right: convertInchesToTwip(0.1)
+    }
+  });
+
+  var three = new TableCell({
+    children: arrayThree,
+    width: {
+      size: 33.3,
+      type: WidthType.PERCENTAGE
+    },
+    height: {
+      size: 0.2
+    },
+    margins: {
+      left: convertInchesToTwip(0.1),
+      right: convertInchesToTwip(0.1)
+    },
+    borders: {
+      right: {
+        size: 3
+      }
+    }
+  });
 
   const tableRow = new TableRow({
     children: [one, two, three]
   });
 
-  return new docx.Table({ rows: [tableRow] });
+  return new docx.Table({
+    alignment: AlignmentType.CENTER,
+    rows: [tableRow],
+    width: {
+      size: 100,
+      type: WidthType.PERCENTAGE
+    },
+    borders: {
+      top: {
+        size: 3
+      },
+      bottom: {
+        size: 3
+      }
+    }
+  });
 }
