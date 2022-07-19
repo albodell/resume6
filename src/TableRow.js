c3487c9250bed6df5328cedfa8fa9c9c225e073b
albodell <106753318+albodell@users.noreply.github.com>
2022-07-19 15:14:21 -0400

    tabel

--- src/TableRow.js
+++ src/TableRow.js
@@ -2,69 +2,44 @@ import { TableRow, TableCell, Paragraph } from "docx";
 
 export default function getTextPara(textElement) {
   var docx = require("docx");
-  var text;
   var cleanTextArray = textElement.split("\n");
 
-  var arrayOne = [
-    new docx.Paragraph({
-      text: "butt"
-    }),
-    new docx.Paragraph({
-      text: "googer"
-    })
-  ];
-  var arrayTwo = [
-    new docx.Paragraph({
-      text: "goofy"
-    }),
-    new docx.Paragraph({
-      text: "bingus"
-    })
-  ];
+  var arrayOne = [];
+  var arrayTwo = [];
   var arrayThree = [];
 
-  // for (var i = 0; i < cleanTextArray.length; ) {
-  //   text = new docx.TextRun({
-  //     text: cleanTextArray[i]
-  //   });
+  for (var i = 0; i < cleanTextArray.length; ) {
+    arrayOne.push(
+      new docx.Paragraph({
+        text: cleanTextArray[i]
+      })
+    );
+    i++;
+    if (i >= cleanTextArray.length) {
+      break;
+    }
 
-  // one.addChildElement(text);
-  // text = new docx.Paragraph({
-  //   text: "",
-  //   break: 1,
-  //   bullet: {
-  //     level: 0
-  //   }
-  // });
-  // one.addChildElement(text);
-  // i++;
+    arrayTwo.push(
+      new docx.Paragraph({
+        text: cleanTextArray[i]
+      })
+    );
+    i++;
+    if (i >= cleanTextArray.length) {
+      break;
+    }
 
-  // two.addChildElement(text);
-  // text = new docx.Paragraph({
-  //   text: "",
-  //   break: 1,
-  //   bullet: {
-  //     level: 0
-  //   }
-  // });
-  // two.addChildElement(text);
-  // i++;
-
-  // three.addChildElement(text);
-  // text = new docx.Paragraph({
-  //   text: "",
-  //   break: 1,
-  //   bullet: {
-  //     level: 0
-  //   }
-  // });
-  // three.addChildElement(text);
-  // i++;
-  // }
+    arrayThree.push(
+      new docx.Paragraph({
+        text: cleanTextArray[i]
+      })
+    );
+    i++;
+  }
 
   var one = new TableCell({ children: arrayOne });
   var two = new TableCell({ children: arrayTwo });
-  var three = new TableCell({ children: [] });
+  var three = new TableCell({ children: arrayThree });
 
   const tableRow = new TableRow({
     children: [one, two, three]
